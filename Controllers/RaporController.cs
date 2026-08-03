e8bcdb28e6bf2ae5f72d63961c796755353ac562
hayrunisaozcan777-a11y <hayrunisaozcan777@gmail.com>
2026-08-03 17:36:05 +0300

    Raporlar sayfasindaki personel filtresi arama kutusuyla uyumlu hale getirildi

--- Controllers/RaporController.cs
+++ Controllers/RaporController.cs
@@ -11,18 +11,32 @@ namespace DemirbasTakip.Controllers
         private readonly ApplicationDbContext _context;
         public RaporController(ApplicationDbContext context) { _context = context; }
 
-        public async Task<IActionResult> Index(int? personelId, int? demirbasId, DateTime? baslangic, DateTime? bitis)
+        public async Task<IActionResult> Index(string personelAdi, int? demirbasId, DateTime? baslangic, DateTime? bitis)
         {
             var query = _context.Zimmetler.Include(z => z.Personel).Include(z => z.Demirbas).AsQueryable();
 
-            if (personelId.HasValue) query = query.Where(z => z.PersonelId == personelId.Value);
-            if (demirbasId.HasValue) query = query.Where(z => z.DemirbasId == demirbasId.Value);
-            if (baslangic.HasValue) query = query.Where(z => z.ZimmetTarihi.Date >= baslangic.Value.Date);
-            if (bitis.HasValue) query = query.Where(z => z.ZimmetTarihi.Date <= bitis.Value.Date);
+            if (!string.IsNullOrEmpty(personelAdi))
+            {
+                query = query.Where(z => z.Personel.AdSoyad.Contains(personelAdi));
+            }
+
+            if (demirbasId.HasValue)
+            {
+                query = query.Where(z => z.DemirbasId == demirbasId.Value);
+            }
+
+            if (baslangic.HasValue)
+            {
+                query = query.Where(z => z.ZimmetTarihi.Date >= baslangic.Value.Date);
+            }
+
+            if (bitis.HasValue)
+            {
+                query = query.Where(z => z.ZimmetTarihi.Date <= bitis.Value.Date);
+            }
 
             var sonuclar = await query.OrderByDescending(z => z.ZimmetTarihi).ToListAsync();
 
-            ViewBag.Personeller = new SelectList(await _context.Personeller.OrderBy(p => p.AdSoyad).ToListAsync(), "Id", "AdSoyad", personelId);
             ViewBag.Demirbaslar = new SelectList(await _context.Demirbaslar.OrderBy(d => d.Ad).ToListAsync(), "Id", "Ad", demirbasId);
             ViewBag.Baslangic = baslangic?.ToString("yyyy-MM-dd");
             ViewBag.Bitis = bitis?.ToString("yyyy-MM-dd");
